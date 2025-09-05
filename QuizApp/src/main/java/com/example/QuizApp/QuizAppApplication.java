652e6ed2145b3a8bae0f8efd63cc51b516125f28
Rajesh L <rajeshlogu321@gmail.com>
2025-09-06 00:10:39 +0530

    changes

--- QuizApp/src/main/java/com/example/QuizApp/QuizAppApplication.java
+++ QuizApp/src/main/java/com/example/QuizApp/QuizAppApplication.java
@@ -1,15 +1,17 @@
 package com.example.QuizApp;
 
-
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:.env")
 public class QuizAppApplication {
 
 	public static void main(String[] args) {
+
+
 		SpringApplication.run(QuizAppApplication.class, args);
-
 	}
-
 }
